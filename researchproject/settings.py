a0edf15dccf31bbc01d2fd9eed635c89b57df649
Olga <>
2021-08-11 23:41:03 +0300

    change deploy settings

--- researchproject/settings.py
+++ researchproject/settings.py
@@ -28,9 +28,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar'
 ]
-if DEBUG:
-    INSTALLED_APPS += 'debug_toolbar'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
